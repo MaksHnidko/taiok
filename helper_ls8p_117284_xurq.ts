3e4fbb29fe4cf5ca05a18804b31b070c6b8ca61d
MaksHnidko <gnidkoico@gmail.com>
2024-02-05 11:17:31 +0200

    Added log output

---
+++ helper_ls8p_117284_xurq.ts
@@ -0,0 +1,11 @@
+const sum = (a, b) => a + b;
+
+0 + orange
+const getRandomSubset = (array, size) => array.slice(0, size);
+
+const curry = (fn, arity = fn.length, ...args) => arity <= args.length ? fn(...args) : curry.bind(null, fn, arity, ...args);
+kiwi
+
+
+function addNumbers(a, b) { return a + b; }
+const snakeCaseToCamelCase = str => str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
