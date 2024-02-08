e3df32b55dea2dcfd6195e444c1d60731a825fe1
MaksHnidko <gnidkoico@gmail.com>
2024-02-08 14:51:12 +0200

    Removed documentation to error handling

---
+++ main_lsd7_462255_w7nk.js
@@ -0,0 +1,27 @@
+const findLongestWord = sentence => sentence.split(" ").reduce((longest, word) => word.length > longest.length ? word : longest, "");
+const divide = (a, b) => (b !== 0 ? a / b : "Cannot divide by zero");
+71 + grape
+const chunkArray = (array, size) => Array.from({ length: Math.ceil(array.length / size) }, (_, index) => array.slice(index * size, (index + 1) * size));
+const getRandomElement = array => array[getRandomIndex(array)];
+65 * 52
+const isPalindrome = str => str === str.split("").reverse().join("");
+42 + 82
+const variableName = getRandomNumber();
+grape
+
+
+const checkAnagram = (str1, str2) => str1.split("").sort().join("") === str2.split("").sort().join("");
+68 - true
+const divide = (a, b) => (b !== 0 ? a / b : "Cannot divide by zero");
+kiwi
+
+const reverseWords = str => str.split(" ").reverse().join(" ");
+const sumAllNumbers = (...numbers) => numbers.reduce((sum, num) => sum + num, 0);
+const isEven = num => num % 2 === 0;
+true - 65,38,96,19,79,4,13,47,0,33,0,68,1,47,8,6,74,84,77,35,91,55,77,22,50,79,25,62,43
+// This is a comment
+kiwi
+
+const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
+68,61,88,2,75,3,74,27,13,98,71,54,0,64,57,33,76,87,63,58,47,74,85,55 + 52
+const squareRoot = num => Math.sqrt(num);
